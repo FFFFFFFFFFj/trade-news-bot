433504701fa10c1b8f0e2bd95461eab9287bbb23
root <root@vm3502520.firstbyte.club>
2025-08-31 20:41:52 +0000

    add rss

--- bot/handlers.go
+++ bot/handlers.go
@@ -1,8 +1,11 @@
 package bot
 
 import (
+	"fmt"
 	"log"
 	"strings"
+
+	"github.com/FFFFFFFFFFj/trade-news-bot/rss"
 )
 
 func (b *Bot) HandleMessage(m *Message) {
@@ -11,7 +14,25 @@ func (b *Bot) HandleMessage(m *Message) {
 	case "/start":
 		b.SendMessage(m.Chat.ID, "Hi, I'm bot. Write /latest")
 	case "/latest":
-		b.SendMessage(m.Chat.ID, "This is news")
+		items, err := rss.Fetch("https://www.investing.com/rss/news.rss")
+		if err != nil {
+			b.SendMessage(m.Chat.ID, "Error loading news : (")
+			return
+		}
+
+		limit := 3
+		if len(items) < limit {
+			limit = len(items)
+		}
+
+		var sb strings.Builder
+		for i := 0; i < limit; i++ {
+			sb.WriteString(fmt.Sprintf("* %s\n%s\n\n", items[i].Title, items[i].Link))
+		}
+
+		b.SendMessage(m.Chat.ID, sb.String())
+
+		
 	default:
 		log.Printf("Got message: %s", txt)
 	}
---
+++ rss/rss.go
@@ -0,0 +1,42 @@
+package rss
+
+import (
+	"encoding/xml"
+	"net/http"
+	"time"
+)
+
+//HTTP client with timeout
+var client = &http.Client{Timeout: 15 * time.Second}
+
+//Structures for RSS parsing
+type RSS struct {
+	XMLName xml.Name `xml:"rss"`
+	Channel Channel  `xml:"channel"`
+}
+
+type Channel struct {
+	Title string `xml:"title"`
+	Items []Item `xml:"item"`
+}
+
+type Item struct {
+	Title   string `xml:"title"`
+	Link    string `xml:"link"`
+	PubDate string `xml:"pubDate"`
+}
+
+//Fetch loads the RSS and returns a list of news items
+func Fetch(url string) ([]Item, error) {
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var rss RSS
+	if err := xml.NewDecoder(resp.Body).Decode(&rss); err != nil {
+		return nil, err
+	}
+	return rss.Channel.Items, nil
+}
