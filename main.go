3d5b658c2e3aa95447974295542cc469cc387091
Igor <49080856+FFFFFFFFFFj@users.noreply.github.com>
2025-09-13 17:06:56 +0500

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/FFFFFFFFFFj/trade-news-bot/bot"
 	"github.com/FFFFFFFFFFj/trade-news-bot/storage"
 )
-
 
 func main() {
 	token := os.Getenv("BOT_TOKEN")
@@ -23,9 +23,29 @@ func main() {
 
 	err = storage.Migrate(db)
 	if err != nil {
-		log.Fatal("Migration faild:", err)
+		log.Fatal("Migration failed:", err)
 	}
 
 	b := bot.New(token, db)
+
+	// News sources
+	sources := []string{
+		"https://www.finmarket.ru/rss/main.asp",
+		"https://www.finmarket.ru/rss/ecworld.asp",
+		"https://www.finmarket.ru/rss/finances.asp",
+		"https://www.bfm.ru/rss/news.xml",
+		"https://rssexport.rbc.ru/rbcnews/economics/full.rss",
+		"https://rssexport.rbc.ru/rbcnews/finance/full.rss",
+		"https://rssexport.rbc.ru/rbcnews/business/full.rss",
+		"https://www.interfax.ru/rss.asp",
+		"https://tass.ru/rss/v2/economy.xml",
+		"https://ru.investing.com/rss/news.rss",
+		"https://ru.investing.com/rss/forex.rss",
+		"https://ru.investing.com/rss/cryptocurrency.rss",
+	}
+
+	// Run background news update (every 10 minutes)
+	b.StartNewsUpdater(sources, 10*time.Minute)
+
 	b.Start()
 }
