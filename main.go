d873b0bdc30139fbb50ce1d51945b22cd39fffe2
Igor <49080856+FFFFFFFFFFj@users.noreply.github.com>
2025-09-15 20:41:41 +0500

    Update main.go

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	}
 
 	// Запускаем обновление новостей (внутри уже запускается goroutine)
-	b.StartNewsUpdater(sources, 10*time.Minute)
+	b.StartNewsUpdater(10 * time.Minute)
 
 	// Запускаем рассылки 3 раза в день: пример 09:00, 15:00, 21:00 — и берём новости за последние 8 часов
 	b.StartBroadcastScheduler([]string{"09:00", "15:00", "21:00"}, 8*time.Hour)
