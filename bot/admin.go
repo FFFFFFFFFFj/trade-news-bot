a3fa197a05a251469332a24ef6307c6f2431786e
Igor <49080856+FFFFFFFFFFj@users.noreply.github.com>
2025-09-19 15:14:37 +0500

    Create admin.go

---
+++ bot/admin.go
@@ -0,0 +1,19 @@
+package bot
+
+import (
+	"log"
+
+	"github.com/FFFFFFFFFFj/trade-news-bot/storage"
+)
+
+func (b *Bot) AdminBroadcast(msg string) {
+	users, err := storage.GetAllUsers(b.db)
+	if err != nil {
+		log.Printf("Ошибка выборки пользователей: %v", err)
+		return
+	}
+
+	for _, u := range users {
+		b.SendMessage(u.ID, "📢 "+msg)
+	}
+}
