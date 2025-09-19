7eb57d7f44b9081f972b39213c690accdceceb92
Igor <49080856+FFFFFFFFFFj@users.noreply.github.com>
2025-09-19 16:21:20 +0500

    Update autopost.go

--- bot/autopost.go
+++ bot/autopost.go
@@ -9,16 +9,10 @@ func (b *Bot) ShowAutopostMenu(chatID int64) {
 	menu := &tb.ReplyMarkup{}
 	var rows [][]tb.InlineButton
 
+	// создаём кнопки выбора времени
 	for hour := 0; hour < 24; hour++ {
-		// создаём кнопку через меню
 		btn := menu.Data(fmt.Sprintf("%02d:00", hour), fmt.Sprintf("ap_%02d00", hour))
-		// преобразуем tb.Btn в tb.InlineButton
-		rows = append(rows, []tb.InlineButton{
-			tb.InlineButton{
-				Unique: btn.Unique,
-				Text:   btn.Text,
-			},
-		})
+		rows = append(rows, []tb.InlineButton{btn}) // теперь просто btn, без разыменования
 	}
 	menu.InlineKeyboard = rows
 
