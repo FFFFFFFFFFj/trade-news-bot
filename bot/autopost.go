2eea59ab6c62a0504e8cca64ccb16d82f3b7ec1b
Igor <49080856+FFFFFFFFFFj@users.noreply.github.com>
2025-09-19 15:13:25 +0500

    Create autopost.go

---
+++ bot/autopost.go
@@ -0,0 +1,22 @@
+package bot
+
+import (
+	"fmt"
+	tb "gopkg.in/telebot.v3"
+)
+
+func (b *Bot) ShowAutopostMenu(chatID int64) {
+	menu := &tb.ReplyMarkup{}
+	var rows [][]tb.InlineButton
+	for hour := 0; hour < 24; hour++ {
+		btn := menu.Data(fmt.Sprintf("%02d:00", hour), fmt.Sprintf("ap_%02d00", hour))
+		rows = append(rows, []tb.InlineButton{btn})
+	}
+	menu.InlineKeyboard = rows
+
+	_, _ = b.bot.Send(
+		tb.ChatID(chatID),
+		"Выберите время авторассылки (по Москве):",
+		menu,
+	)
+}
